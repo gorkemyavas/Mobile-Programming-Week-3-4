b8941135cb72b4628b17838c07493237aad62028
gorkem <gorkemyavas98@gmail.com>
2022-03-17 12:33:19 +0300

    Add project files.

---
+++ Mobile-Programming-Week-3-4/Mobile-Programming-Week-3-4/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using Xamarin.Forms;
+
+namespace Mobile_Programming_Week_3_4
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            var stackLayout = new StackLayout();
+            stackLayout.Orientation = StackOrientation.Vertical;
+
+            BindingFlags bindingFlags = BindingFlags.Public | BindingFlags.GetProperty
+                | BindingFlags.Static;
+            foreach (var field in typeof(Color).GetFields(bindingFlags))
+            {
+                var frame = new Frame
+                {
+                    BackgroundColor = (Color)field.GetValue(null),
+                    Content = new Label
+                    {
+                        Text = field.ToString()
+                    }
+                };
+                stackLayout.Children.Add(frame);
+            }
+
+
+            Content = stackLayout;
+            InitializeComponent();
+        }
+
+    }
+}
